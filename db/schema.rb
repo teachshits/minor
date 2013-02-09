eb22a00bd654d9eb17aa8adb6215ab30486ff22e
Stanislav Sobolev <iamjacke@gmail.com>
2013-02-09 09:27:51 -0800

    heroku deploy

--- db/schema.rb
+++ db/schema.rb
@@ -29,6 +29,15 @@ ActiveRecord::Schema.define(:version => 20111122232140) do
     t.datetime "updated_at"
   end
 
+  create_table "movies", :force => true do |t|
+    t.string   "title"
+    t.string   "rating"
+    t.text     "description"
+    t.datetime "release_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "schedules", :force => true do |t|
     t.integer  "employee_id"
     t.integer  "week_id"
