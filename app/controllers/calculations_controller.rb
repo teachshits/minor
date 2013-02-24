0826d6476f06a9af4a8e456ec2c4e18b24678b6b
Stanislav Sobolev <iamjacke@gmail.com>
2013-02-23 20:06:28 -0800

    Calculation sidebar + user with rank

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -6,6 +6,11 @@ def index
 	@positions = Position.find(14)
 	@emp = Employee.find_all_by_position_id(14)
 	@quantity = ApprPosition.find_by_position_id(14).Mon
+	# Rails 4
+	# @emp_r1 = Employee.where(position_id: 14).where(rank: 1)
+	@emp_r1 = Employee.find_all_by_position_id_and_rank(14, 1)
+	@emp_r2 = Employee.find_all_by_position_id_and_rank(14, 2)
+	@emp_r3 = Employee.find_all_by_position_id_and_rank(14, 3)
 end
 
 end
