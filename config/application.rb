3358a064a30d6852d6aacdedea782be230fe1daf
Stanislav Sobolev <iamjacke@gmail.com>
2013-03-18 11:21:14 -0700

    Grammar + info

--- config/application.rb
+++ config/application.rb
@@ -38,11 +38,11 @@ module Minority
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    
     config.assets.prefix = Rails.root.join('tmp/assets').to_s
     # Enable the asset pipeline
     config.assets.enabled = true
     config.assets.initialize_on_precompile = false
-    #config.assets.precompile += %w( *.css *.js )
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
