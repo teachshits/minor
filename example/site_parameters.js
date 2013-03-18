9ed3bf5caff13368ffc481a2f21b47e10491d554
Stanislav Sobolev <iamjacke@gmail.com>
2013-03-18 02:18:51 -0700

    Deploy fixes

---
+++ example/site_parameters.js
@@ -0,0 +1,36 @@
+/* Javascript Document for site specific parameters */
+site_title = "Library Scheduler";
+
+// Sets current domain name from URL of document
+var domain = document.domain;
+
+// Is top navigation bar set to McMaster global navigation (true) or to local website navigation (false)?
+var global_nav = true;
+
+// Is AJAX used to get top navigation bar?
+var top_nav_ajax = true;
+
+// Sets id where AJAX puts top navigation bar
+var top_nav_id="top_navigation_bar";
+
+// Activates first skip nav link for ARIA navigation
+var skipnavlink_on = true;
+
+// Sets title of navigation bar for first skip nav link
+var skipnavlink_title = "top";
+
+// Activates second skip nav link for ARIA navigation
+var skipnav2link_on = true;
+
+// Sets title of navigation bar for second skip nav link
+var skipnav2link_title = "left";
+
+// Sets order of skip nav links: true = first skip nav is display first; false = second skip nav is displayed first
+var top_nav_anchor_first = true;
+
+// Activates automatic drop down for left navigation
+var automatic_left_nav = true;
+
+// Sets URL that AJAX uses to get the top navigation bar file
+var top_nav_url="";
+
